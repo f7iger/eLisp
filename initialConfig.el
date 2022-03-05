6458e33f3fc75db13da1b8b70e2094d9a8ee4dbd
Fernando Alves <67766657+f7iger@users.noreply.github.com>
2022-03-05 14:31:39 -0300

    Configurações iniciais
    Remoção de toolbars e customizações gerais

---
+++ initialConfig.el
@@ -0,0 +1,16 @@
+;; Remover boas vindas
+;; (setq inhibit-starttup-message t)
+
+;; Remover menus
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; Remover barra de rolagem
+(scroll-bar-mode -1)
+
+;; Numerar linhas
+;; (global-linum-mode t)
+
+;; Tamanho da fonte
+;; (set-face-attribute 'default nil :height 100')
+
