b195753a0e30f4e24bc914cbd6bfd8ee786b5c0a
Fernando Alves <67766657+f7iger@users.noreply.github.com>
2022-06-13 13:19:34 +0000

    Basic themes for emacs
    - Cyberpunk theme;
    - Ample theme;
    - Clues theme;
    - Gruber dark theme.

---
+++ Themes.el
@@ -0,0 +1,16 @@
+;; [***Themes***]
+;; Cyberpunk theme:
+(add-to-list 'custom-theme-load-path' "~/.emacs.d/themes/cyberpunk-theme.el")
+(load-theme 'cyberpunk t)
+
+;; Ample theme:
+(add-to-list 'custom-theme-load-path' "~/.emacs.d/themes/ample-theme.el")
+(load-theme 'ample-zen t)
+
+;; Clues theme:
+(add-to-list 'custom-theme-load-path' "~/.emacs.d/themes/clues-theme.el")
+(load-theme 'clues t)
+
+;; Gruber darker theme:
+(add-to-list 'custom-theme-load-path' "~/.emacs.d/themes/gruber-darker-theme.el")
+(load-theme 'gruber-darker t)
